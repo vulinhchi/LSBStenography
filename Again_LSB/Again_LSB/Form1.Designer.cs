b2af125a7caa31ccf2d5143190803ea899c0dfd8
VuLinhChi <vulinhchi.05@gmail.com>
2018-04-07 14:51:47 +0700

    Vũ Linh Chi 2:51PM 7_4_2018

---
+++ Again_LSB/Again_LSB/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Again_LSB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1_chonanh = new System.Windows.Forms.Button();
+            this.Btn_Encode = new System.Windows.Forms.Button();
+            this.Btn_Decode = new System.Windows.Forms.Button();
+            this.text_Mess = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(93, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(423, 238);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            //this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1_chonanh
+            // 
+            this.button1_chonanh.Location = new System.Drawing.Point(12, 12);
+            this.button1_chonanh.Name = "button1_chonanh";
+            this.button1_chonanh.Size = new System.Drawing.Size(75, 23);
+            this.button1_chonanh.TabIndex = 1;
+            this.button1_chonanh.Text = "Chọn ảnh";
+            this.button1_chonanh.UseVisualStyleBackColor = true;
+            this.button1_chonanh.Click += new System.EventHandler(this.button1_chonanh_Click);
+            // 
+            // Btn_Encode
+            // 
+            this.Btn_Encode.Location = new System.Drawing.Point(12, 59);
+            this.Btn_Encode.Name = "Btn_Encode";
+            this.Btn_Encode.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Encode.TabIndex = 2;
+            this.Btn_Encode.Text = "Encode";
+            this.Btn_Encode.UseVisualStyleBackColor = true;
+            this.Btn_Encode.Click += new System.EventHandler(this.Btn_Encode_Click);
+            // 
+            // Btn_Decode
+            // 
+            this.Btn_Decode.Location = new System.Drawing.Point(12, 110);
+            this.Btn_Decode.Name = "Btn_Decode";
+            this.Btn_Decode.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Decode.TabIndex = 3;
+            this.Btn_Decode.Text = "Decode";
+            this.Btn_Decode.UseVisualStyleBackColor = true;
+            this.Btn_Decode.Click += new System.EventHandler(this.Btn_Decode_Click);
+            // 
+            // text_Mess
+            // 
+            this.text_Mess.Location = new System.Drawing.Point(93, 265);
+            this.text_Mess.Name = "text_Mess";
+            this.text_Mess.Size = new System.Drawing.Size(291, 20);
+            this.text_Mess.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 265);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Message";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 306);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.text_Mess);
+            this.Controls.Add(this.Btn_Decode);
+            this.Controls.Add(this.Btn_Encode);
+            this.Controls.Add(this.button1_chonanh);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1_chonanh;
+        private System.Windows.Forms.Button Btn_Encode;
+        private System.Windows.Forms.Button Btn_Decode;
+        private System.Windows.Forms.TextBox text_Mess;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Again_LSB/Again_LSB/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Again_LSB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static int ReverseBits(int n)
+        {
+            int result = 0;
+
+            for (int i = 0; i < 8; i++)
+            {
+                result = result * 2 + n % 2;
+
+                n /= 2;
+            }
+
+            return result;
+        }
+        private void button1_chonanh_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open_dialog = new OpenFileDialog();
+            open_dialog.Filter = "Image Files (*.jpeg; *.png; *.bmp)|*.jpg; *.png; *.bmp";
+
+            if (open_dialog.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(open_dialog.FileName);
+            }
+            text_Mess.Text = "";
+        }
+
+        public static Bitmap Encode(String messa, Bitmap img)
+        {
+            int PixelIndex = 0;
+            int Done = 0;// xét trạng thái có mã hóa tiếp hay không
+            int msgIndex = 0;
+            int charValueimg = 0;
+
+            int count = 0;
+            int R = 0, G = 0, B = 0;
+
+            for (int i = 0; i < img.Height; i++)
+            {
+                for (int j = 0; j < img.Width; j++)
+                {
+                    Color pixel = img.GetPixel(j, i);//Get pixel at position(j,i)
+                    R = pixel.R - pixel.R % 2;//set LSB của R thành bit chẵn
+                    G = pixel.G - pixel.G % 2;
+                    B = pixel.B - pixel.B % 2;
+                    for (int n = 0; n < 3; n++)
+                    {
+                        if (PixelIndex % 8 == 0)
+                        {
+
+                            if (Done == 1 && count == 8)
+                            {
+
+                                if ((PixelIndex - 1) % 3 < 2)
+                                {
+                                    img.SetPixel(j, i, Color.FromArgb(R, G, B));
+                                }
+                                
+                            }
+                            if (msgIndex >= messa.Length)
+                            {
+                                Done = 1;
+                            }
+                            else
+                            {
+                                charValueimg = messa[msgIndex++];
+                            }
+                        }
+                        switch (PixelIndex % 3)
+                        {
+                            case 0:
+                                {
+                                    if (Done == 0)
+                                    {
+
+                                        R += charValueimg % 2;
+                                        charValueimg /= 2;
+                                    }
+                                }
+                                break;
+                            case 1:
+                                {
+                                    if (Done == 0)
+                                    {
+                                        G += charValueimg % 2;
+                                         charValueimg /= 2;
+                                    }
+                                }
+                                break;
+                            case 2:
+                                {
+                                    if (Done == 0)
+                                    {
+                                        B += charValueimg % 2;
+                                            charValueimg /= 2;
+                                    }
+
+                                    img.SetPixel(j, i, Color.FromArgb(R, G, B));
+                                }
+                                break;
+                        }
+                        PixelIndex++;
+                        if (Done == 1)
+                        {
+
+                            count++;
+                        }
+                    }
+                }
+            }
+            return img;
+        }
+        private void Btn_Encode_Click(object sender, EventArgs e)
+        {
+            Bitmap img = (Bitmap)pictureBox1.Image;// lấy ảnh ra
+            String messa = text_Mess.Text;// lấy dữ liệu trong ô text mess ra
+            // kết quả
+            Bitmap ketqua = Encode( messa, img);
+
+            // lưu kết quả
+            pictureBox1.Image = ketqua;
+            SaveFileDialog saveFile = new SaveFileDialog();
+            //save_dialog.Filter = "Png Image|*.png|Bitmap Image|*.bmp";
+            saveFile.Filter = "Image Files (*.png, *.bmp) | *.png; *.bmp";
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                switch (saveFile.FilterIndex)
+                {
+                    case 0:
+                        {
+                            img.Save(saveFile.FileName, ImageFormat.Png);
+                        }
+                        break;
+                    case 1:
+                        {
+                            img.Save(saveFile.FileName, ImageFormat.Bmp);
+                        }
+                        break;
+                }
+
+            }
+            text_Mess.Text = "";
+
+        }
+
+        public static String Decode(Bitmap img)
+        {
+            int PixelIndex = 0;
+            int charValue = 0;
+            string msg = "";
+            for (int i = 0; i < img.Height; i++)
+            {
+                for (int j = 0; j < img.Width; j++)
+                {
+                    Color pixel = img.GetPixel(j, i);
+                    for (int n = 0; n < 3; n++)
+                    {
+                        switch (PixelIndex % 3)
+                        {
+                            case 0:
+                                {
+                                    charValue = charValue * 2 + pixel.R % 2;
+                                }
+                                break;
+                            case 1:
+                                {
+                                    charValue = charValue * 2 + pixel.G % 2;
+                                }
+                                break;
+                            case 2:
+                                {
+                                    charValue = charValue * 2 + pixel.B % 2;
+                                }
+                                break;
+                        }
+
+                        PixelIndex++;
+
+                        if (PixelIndex % 8 == 0)
+                        {
+
+                            charValue = ReverseBits(charValue);
+                            if (charValue == 0)
+                               return msg;
+                            char word = (char)charValue;
+                            //msg += word.ToString();
+                            msg += word;
+                        }
+                    }
+                }
+            }
+            return msg;
+        }
+
+         
+    
+
+        private void Btn_Decode_Click(object sender, EventArgs e)
+        {
+            Bitmap img = (Bitmap)pictureBox1.Image;// lấy ảnh ra
+              String ketqua = Decode(img);
+                text_Mess.Text = ketqua;
+        }
+
+       
+    }
+}
